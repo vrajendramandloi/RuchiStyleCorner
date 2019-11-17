3e4c6bf8521978f7a1b035a167c41a28f718fa39
imvrajendra@gmail.com <imvrajendra@gmail.com>
2019-11-17 10:13:29 +0000

    Navbar with menu functional now.

--- src/app/controllers/navbarController/navbar.component.ts
+++ src/app/controllers/navbarController/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component ({
   selector: 'navbar-component',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  ngOnInit() {
+    $(document).ready(function(){
+      $('.navbar-toggle a').click(function() {
+        $('.navbar-menu').toggleClass('active');
+      })
+    });
+  }
 }

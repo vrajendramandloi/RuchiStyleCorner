bdbe7c438a8180df21b8208c8f8e46efbd6d68e4
imvrajendra@gmail.com <imvrajendra@gmail.com>
2020-06-20 14:39:02 +0100

    added scrolling smooth to Home page with contact details.

--- src/app/controllers/homeController/home.component.ts
+++ src/app/controllers/homeController/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
-@Component ({
+@Component({
   selector: 'home-component',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  constructor() {
+
+  }
+
+  scrollToBottom() {
+    $('html,body').animate({scrollTop: document.body.scrollHeight}, 'slow');
+  }
 }
